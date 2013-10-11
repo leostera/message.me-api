81114aa54d80e0f182830c26ce641928c6481d3a
Leandro Ostera <me@leostera.com>
2013-10-11 08:59:20 -0300

    fix(injector): Trims whitespace on getParams.
    Damn whitespace. Damn you so much.

--- utils/injector.js
+++ utils/injector.js
@@ -1,32 +1,23 @@
-var util = require('util');
+var util = require('util')
+  , _ = require('lodash')
 
-var registry = {};
+var registry = exports.registry = {};
 
 var getParams = function (fn) {
   var params = [];
+  var fnStr = fn.toString();
   /* this regexes originally from the angular source */
   var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
   var FN_ARG_SPLIT = /,/;
   var FN_ARG = /^\s*(_?)(.+?)\1\s*$/;
   var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
-  var fnText = fn.toString().replace(STRIP_COMMENTS, '');
+  var fnText = fnStr.replace(STRIP_COMMENTS, '');
   var argDecl = fnText.match(FN_ARGS);
   argDecl[1].split(FN_ARG_SPLIT).forEach(function (arg){
-    params.push(arg)
+    params.push(arg.trim())
   });
-  return params;
-}
 
-/**
- * lookup
- * @param  {dep} dep [description]
- * @return {[type]}     [description]
- */
-var lookup = function (dep) {
-  if(registry[dep] === 'undefined') {
-    throw dep+' was not loaded in the injector!';
-  }
-  return registry[dep.toString()];
+  return params;
 }
 
 /**
@@ -34,17 +25,27 @@ var lookup = function (dep) {
  * @param  {Function} fn the function in which to inject stuff
  * @return {?}      whatever the function returns
  */
-exports.inject = function (fn) {
+var inject = exports.inject = function (fn) {
+  if(typeof fn !== 'function') {
+    return fn;
+  }
+
   if(!Object.keys(registry).length) {
     throw 'No dependencies registered for injection!';
   }
 
-  deps = getParams(fn).map(function (param) {
-    return lookup(param);
-  });
+  var params = getParams(fn);
+  if(!params.length) {
+    return fn();
+  }
+
+  var deps = params.map(function (param) {
+    return registry[param];
+  }).map(function (dep) {
+    return inject(dep);
+  })
 
   switch (deps.length) {
-    case 0: return fn();
     case 1: return fn(deps[0]);
     case 2: return fn(deps[0], deps[1]);
     case 3: return fn(deps[0], deps[1], deps[2]);
@@ -56,24 +57,22 @@ exports.inject = function (fn) {
   }
 }
 
+/**
+ * load
+ * @param  {Object} deps the dependencies to load
+ */
 exports.load = function (deps) {
   if(!util.isArray(deps)) {
     deps = [deps];
   }
-
   deps.forEach(function (dep) {
     if(dep.wrapAs) {
       registry[dep.wrapAs] = dep.obj;
       return;
     }
-    // let's just add this keys to the registry one by one
-    for(var key in dep) {
-      if(registry[key] !== undefined) {
-        throw 'Name collision!\n'+'Object '+key+'\n'+dep[key]+'is already registered as object: '+registry[key];
-      }
-      if(dep.hasOwnProperty(key)) {
-        registry[key] = dep[key];
-      }
-    }
+    // let's just add this keys to the registry
+    Object.keys(dep).forEach(function (key) {
+      registry[key] = dep[key];
+    });
   });
 }
