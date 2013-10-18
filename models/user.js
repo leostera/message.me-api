8e7acb9e0229d86c08da0234b58ed7be0eab30b9
Leandro Ostera <me@leostera.com>
2013-10-18 09:04:36 -0300

    Removes conversations and unread_messages from user.

--- models/user.js
+++ models/user.js
@@ -3,11 +3,6 @@ var userModel;
 module.exports = function (mongoose) {
 
   var userSchema = mongoose.Schema({
-      conversations: [mongoose.Schema.Types.ObjectId],
-      unread_messages: [{
-        conversation: mongoose.Schema.Types.ObjectId,
-        message: mongoose.Schema.Types.ObjectId,
-      }],
       username: String,
       email: String,
       facebook: String,
