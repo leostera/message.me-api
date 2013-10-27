6d7cacb81deea0b414642cf0ce582d34e2935b26
Leandro Ostera <me@leostera.com>
2013-10-27 02:15:25 -0300

    Inject ws/index appropriately

--- worker.js
+++ worker.js
@@ -1,6 +1,7 @@
 /**
- *  module dependencies
+ *  Module dependencies.
  */
+
 var express    = require('express');
 var http       = require('http');
 var mongoose   = require('mongoose');
@@ -8,6 +9,9 @@ var passport   = require('passport');
 var path       = require('path');
 var redis      = require('redis');
 var redisStore = require('connect-redis');
+var aws        = require('aws-sdk');
+
+// custom setup of dependencies
 
 redisStore   = redisStore(express);
 express.cors = require('./lib/utils/cors');
@@ -17,6 +21,15 @@ var config   = global.config = require('./config');
 var injector = require('./lib/utils/injector');
 var createClient = require('./lib/utils/redis').createClient;
 
+aws.config.update(config.aws);
+var SQS = new aws.SQS();
+
+config.session.store.prefix = config.session.store.prefix.text +
+    ( config.session.store.prefix.useEnv
+      ? "_"+process.env.NODE_ENV
+      : '');
+var store = new redisStore(config.session.store);
+
 mongoose.connect('mongodb://'
   + config.db.user
   + ((config.db.password) ? ':'+config.db.password : '')
@@ -25,11 +38,12 @@ mongoose.connect('mongodb://'
   + ":"+config.db.port
   + "/"+config.db.dbname);
 
-config.session.store.prefix = config.session.store.prefix.text +
-    ( config.session.store.prefix.useEnv
-      ? "_"+process.env.NODE_ENV
-      : '');
-var store = new redisStore(config.session.store);
+injector.load([
+    {wrapAs: 'Redis', obj: {createClient: createClient}}
+  , {wrapAs: 'Config', obj: config}
+  , {wrapAs: 'SessionStore', obj: store}
+  , {wrapAs: 'SQS', obj: SQS}
+]);
 
 app.set('port', config.server.port);
 app.set('config', config);
@@ -64,4 +78,8 @@ app.use(express.errorHandler());
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+var io = require('./ws');
+io = __inject(io);
+io.start(app, server);
 
