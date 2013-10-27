3e2fdf89427a1055eee20ad7ec92ec36f5de0791
Leandro Ostera <me@leostera.com>
2013-10-26 23:59:23 -0300

    Adds clustered index

---
+++ index.js
@@ -0,0 +1,11 @@
+var cluster = require('cluster');
+
+if(cluster.isMaster) {
+  var cpus = require('os').cpus().length;
+  for (var i = 0; i < cpus; i += 1) {
+    cluster.fork();
+  }
+} else {
+  console.log('Starting worker #', cluster.worker.id);
+  require('./worker');
+}
