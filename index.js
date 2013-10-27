ec26c382b706810c906a116189049a48fb455091
Leandro Ostera <me@leostera.com>
2013-10-27 07:01:25 -0300

    Remap the prefix in the entry point

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ if(cluster.isMaster) {
   }
 } else {
 	global.config = require('./config');
-	config.session.store.prefix = config.session.store.prefix.text +
+	
+  config.session.store.prefix = config.session.store.prefix.text +
     ( config.session.store.prefix.useEnv
       ? process.env.NODE_ENV
       : '') + "_";
+
+  config.io.store.prefix = config.io.store.prefix.text +
+    ( config.io.store.prefix.useEnv
+      ? process.env.NODE_ENV
+      : '') + "_";
+
   console.log('Starting worker #', cluster.worker.id,'– pid:', process.pid);
+  
   var server = require('./worker-http');
+  
   var io = require('./worker-ws');
   io.start(server);
 }
