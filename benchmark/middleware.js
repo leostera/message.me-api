573b60ab490fe70bead4568b84c555dd8eb88328
Leandro Ostera <me@leostera.com>
2013-10-27 23:21:16 -0300

    Adds benchmarking middleware/utils

---
+++ benchmark/middleware.js
@@ -0,0 +1,49 @@
+/**
+ *  Module dependencies.
+ */
+
+var config = global.config || require('../config');
+var ws = require('ws');
+
+var socket;
+
+var connect = function () {
+	socket = new ws('ws://'+config.benchmark.host+":"+config.benchmark.port);
+	socket.on('error', function (err) {
+		socket = false;
+	});
+}
+
+connect();
+
+/**
+ *  Expose the module.
+ */
+
+module.exports = middleware = {};
+
+// send a mark to the server
+
+middleware.send = function (markname) {
+	if(!socket) {
+		connect();
+	}
+	if(!socket) {
+		return;
+	}
+	socket.send(JSON.stringify({
+		mark: markname,
+		time: Date.now()
+	}));
+};
+
+// connect middleware to send a mark
+
+middleware.sendMark = function (markname) {
+	return function (req, res, next) {
+		if(socket) {
+			middleware.send(markname);
+		}
+		next();
+	}
+}
