74ba9929d5ebb8852bc9a5c57dc2f9006db14cc9
Leandro Ostera <me@leostera.com>
2013-10-28 23:58:06 -0300

    Pass in the cluster id to the worker.

--- benchmark/index.js
+++ benchmark/index.js
@@ -2,14 +2,15 @@ var cluster = require('cluster');
 
 global.config = require('../config');
 
+var cpus = require('os').cpus().length;
+
 if(cluster.isMaster) {
-  var cpus = require('os').cpus().length;
   for (var i = 0; i < cpus; i += 1) {
     cluster.fork();
   }
 } else {
   var worker = require('./worker');
   worker.prepare().then(function () {
-    worker.work();
+    worker.work(cluster.worker.id);
   });
 }
